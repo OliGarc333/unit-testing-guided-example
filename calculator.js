cbec3a3278a64603592e83142036c7efc6a15249
Oliver Garcia <ngarcia333@gmail.com>
2022-01-22 13:05:20 +0100

    first iteration

--- calculator.js
+++ calculator.js
@@ -1,16 +1,26 @@
 function sum(a, b) {
-  return;
+  const number1 = a || 0;
+  const number2 = b || 0;
+  return number1 + number2;
 }
 
 function subtract(a, b) {
-  return;
+  const number1 = a || 0;
+  const number2 = b || 0;
+  return number1 - number2;
 }
 
 function divide(a, b) {
-  return;
+  if (a === 0 || b === 0) {
+    return error;
+  }
+  return a / b;
 }
 
 function multiply(a, b) {
+  if (a === 0 || b === 0) {
+    return 0;
+  }
   return a * b;
 }
 
